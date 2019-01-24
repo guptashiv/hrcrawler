ae223033c57afb76e71ad6747d8adc8d3a49d1e8
guptashiv <shivgupt4@gmail.com>
2019-01-24 12:29:02 +0530

    downloads the code in already created folders

--- SelHr.py
+++ SelHr.py
@@ -5,6 +5,35 @@ from getpass import getpass
 import json
 import requests
 
+solURL = 'https://www.hackerrank.com/rest/contests/eso207pa1/submissions/'
+
+#will contain latest submission ids for each user
+StackIDs = {} 
+PrefIDs = {}
+
+def getSubIDs(jdata, idDictionary):	
+	total = jdata['total']
+	for i in range(total):
+		curr = jdata['models'][i]
+		uname = curr['hacker_username']
+		subID = curr['id']
+		if not (uname in idDictionary.keys()):
+			idDictionary[uname] = subID
+
+
+def getCode(subID, uname):
+	# print('subID is', subID )
+	sub = solURL + str(subID)
+	driver.get(sub)
+	jdata = json.loads(driver.find_element_by_id("json").text)
+	curr = jdata['model']	
+	slug = curr['slug']
+	print(['code'], file=open(slug+'/'+uname+'.cpp', "w"))
+	# exctract JSON HERE. jdata['code'] will have the actual code
+
+
+
+
 
 parser = argparse.ArgumentParser(description = "Loging in")
 parser.add_argument("username")
@@ -16,10 +45,9 @@ giturl = 'http://github.com/login'
 hrurl = 'https://www.hackerrank.com/auth/login'
 ccurl = 'https://www.codechef.com/login'
 ccprivate = 'https://www.codechef.com/users/shivgupta/edit'
-hrprivate = 'https://www.hackerrank.com/contests/cs345a18pa1/judge/submissions/challenge/match-me-if-you-can'
+
 
 prefSubmUrl = "https://www.hackerrank.com/rest/contests/eso207pa1/judge_submissions/?offset=0&limit=1000&challenge_id=i-am-prefixed"
-
 stackSubUrl = "https://www.hackerrank.com/rest/contests/eso207pa1/judge_submissions/?offset=0&limit=1000&challenge_id=stack-em-up"
 
 
@@ -40,20 +68,38 @@ login.click()
 
 driver.get(prefSubmUrl)
 prefJson = json.loads(driver.find_element_by_id("json").text)
-print(prefJson['total'])
+# print(prefJson['total'])
 
 
 driver.get(stackSubUrl)
 stackJson = json.loads(driver.find_element_by_id("json").text)
-print(stackJson['total'])
+# print(stackJson['total'])
 
 
-for i in range(prefJson['total']):
-	print(prefJson['models'][i])
-	k = input()
-	if(k=='q'):
-		break
-		
+getSubIDs(stackJson, StackIDs)
+getSubIDs(prefJson, PrefIDs)
+
+
+for k in StackIDs.keys():
+	getCode(StackIDs[k], k)
+
+for k in PrefIDs.keys():
+	getCode(PrefIDs[k], k)
+
+# print(StackIDs)
+
+# for i in range(prefJson['total']):
+# 	curr = prefJson['models'][i]
+# 	uname = curr['hacker_username']
+# 	subID = curr['id']
+# 	if not (uname in PrefIDs.keys()):
+# 		PrefIDs[uname] = subID
+# 	k = input()
+# 	if(k=='q'):
+# 		break
+
+
+
 # print(prefResp.content)
 # print(type(prefResp))
 # solution = driver.get()
